4ab33f86828fd7cbff9a869e7939ba566c1ed6f7
James Harrington <jamesharrington585@hotmail.com>
2023-04-17 00:34:37 -0400

    finished asset caching route

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -42,10 +42,10 @@ module.exports = () => {
         //   },
         // ],
       }),
-      new InjectManifest({
-        swSrc: "./src/sw.js",
-        swDest: "service-worker.js",
-      }),
+      // new InjectManifest({
+      //   swSrc: "./src/src-sw.js",
+      //   swDest: "service-worker.js",
+      // }),
     ],
 
     module: {
@@ -54,10 +54,10 @@ module.exports = () => {
           test: /\.css$/i,
           use: [MiniCssExtractPlugin.loader, "css-loader"],
         },
-        // {
-        //   test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        //   type: "asset/resource",
-        // },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
         {
           test: /\.m?js$/,
           exclude: /(node_modules|bower_components)/,
